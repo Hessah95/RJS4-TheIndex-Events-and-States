a1d852eb8053a7ce301f8fa9e78732542815d15c
Hessah95 <hessah.alhamadi.95@gmail.com>
2019-09-18 16:37:19 +0300

    Hessah & Shereen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Data
 import authors from "./data";
@@ -6,20 +6,42 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
+import AuthorDetail from "./AuthorDetail";
 
-function App() {
-  return (
-    <div id="app" className="container-fluid">
-      <div className="row">
-        <div className="col-2">
-          <Sidebar />
-        </div>
-        <div className="content col-10">
-          <AuthorsList authors={authors} />
+class App extends Component {
+  state = {
+    currentAuthor: null
+  };
+
+  selectAuthor = author => {
+    const newAuthor = author;
+    this.setState({
+      currentAuthor: newAuthor
+    });
+  };
+
+  selectAuthor = author => {
+    const tempAuthor = this.state.authors.find(item => item === author);
+    this.setState({
+      authors: tempAuthor
+    });
+  };
+
+  render() {
+    return (
+      <div id="app" className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+            <Sidebar />
+          </div>
+          <div className="content col-10">
+            <AuthorsList authors={authors} selectAuthor={this.selectAuthor} />
+            <AuthorDetail authors={authors} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
